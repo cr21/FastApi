0e92844d7f246be780ed96e6384022be44a0d101
chirag tagadiya <chiragtagadiya@chirags-MacBook-Pro.local>
2022-10-10 16:19:14 -0400

    Basic API for users and post

---
+++ app/models.py
@@ -0,0 +1,23 @@
+
+from psycopg2 import Timestamp
+from .database import Base
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
+
+class Post(Base):
+    __tablename__='posts'
+
+    id = Column(Integer, primary_key=True,nullable=False )
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    published = Column(Boolean(), server_default='True', nullable=False)
+    created_at= Column(TIMESTAMP(timezone=True),nullable=False, server_default=text('now()'))
+
+
+class User(Base):
+    __tablename__='users'
+    id = id = Column(Integer, primary_key=True,nullable=False )
+    email = Column(String,primary_key=False,nullable=False )
+    password = Column(String, nullable=False)
+    created_at= Column(TIMESTAMP(timezone=True),nullable=False, server_default=text('now()'))
---
+++ app/schemas.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from pydantic import BaseModel, EmailStr
+
+class PostBase(BaseModel):
+    title:str
+    content:str
+    published:bool=True
+    pass
+
+
+class PostCreate(PostBase):
+    pass
+
+
+
+class Post(PostBase):
+    created_at:datetime
+    id:int
+
+    class Config:
+        orm_mode = True
+
+
+
+
+
+class UserCreate(BaseModel):
+    email:EmailStr
+    password:str
+
+    
+
+
+class UserOut(BaseModel):
+    id:int
+    email:EmailStr
+    created_at:datetime
+
+    class Config:
+        orm_mode = True
