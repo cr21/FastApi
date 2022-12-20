284a2a59d7b2640845f5237e7d77f927d7037db5
cr21 <cr.tagadiya@gmail.com>
2022-12-20 17:03:59 -0500

    Voting model added

--- app/models.py
+++ app/models.py
@@ -20,7 +20,13 @@ class Post(Base):
 
 class User(Base):
     __tablename__='users'
-    id = id = Column(Integer, primary_key=True,nullable=False )
+    id = Column(Integer, primary_key=True,nullable=False )
     email = Column(String,primary_key=False,nullable=False )
     password = Column(String, nullable=False)
     created_at= Column(TIMESTAMP(timezone=True),nullable=False, server_default=text('now()'))
+
+
+class Vote(Base):
+    __tablename__='votes'
+    user_id = Column(Integer,ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"),  primary_key=True)
