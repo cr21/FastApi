a626d69fa90327cd420d482c5ec45748ad53f856
cr21 <cr.tagadiya@gmail.com>
2022-12-20 10:34:32 -0500

    Status code updateds

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -21,15 +21,15 @@ def login(user_credentials:OAuth2PasswordRequestForm = Depends(), db:Session=Dep
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
-        detail=f"{user_credentials.email} not found in database")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, 
+        detail=f"Invalid credentials, User not found")
 
     # if user found verify
 
     verified= utils.verify_pwd(user_credentials.password, user.password)
 
     if not verified:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, 
                 detail="credentials not verified")
 
     # create token
