26151583ba96600c0d500a534f8c5924f955fc27
cr21 <cr.tagadiya@gmail.com>
2022-12-20 14:10:28 -0500

    UserId added to Post Model

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 
 from psycopg2 import Timestamp
 from .database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
 
@@ -13,7 +14,9 @@ class Post(Base):
     content = Column(String, nullable=False)
     published = Column(Boolean(), server_default='True', nullable=False)
     created_at= Column(TIMESTAMP(timezone=True),nullable=False, server_default=text('now()'))
-
+    # table_name.column_name
+    owner_id = Column(Integer, ForeignKey("users.id",ondelete="CASCADE" ),nullable=False)
+    owner = relationship("User")
 
 class User(Base):
     __tablename__='users'
