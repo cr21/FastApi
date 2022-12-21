cc6e57642a63affbebac39733e87f991c7a70c04
cr21 <cr.tagadiya@gmail.com>
2022-12-20 20:37:27 -0500

    alembic auto generated script

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from .config import settings
 from .routers import post, user, auth, vote
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 
 
